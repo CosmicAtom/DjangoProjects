72b9cf84cdf56b2c0b1e1bf5141bc75c2b1e082f
saurav rathva <saurav.rathva@ncode.in>
2022-01-31 16:31:30 +0530

    Editing Entries

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,8 +1,19 @@
+from cProfile import label
+from dataclasses import field
+from pyexpat import model
+from tkinter import Widget
 from django import forms
-from .models import Topic
+from .models import Topic, Entry
 
-class  TopicForm(forms.ModelForm):
+class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text' : ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text' : 'Entry:'}
+        widgets = {'text' : forms.Textarea(attrs={'cols' : 80})}
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,11 @@
+from ast import If, Return
+from multiprocessing import context
+import re
 from django.shortcuts import render, redirect
+from django.urls import is_valid_path
 
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -37,4 +41,42 @@ def new_topic(request):
 
     # Display a blank or invalid form.
     context = {'form' : form}
-    return render(request, 'learning_logs/new_topic.html', contextd)
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """ Add a new entry for particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; Process data.
+        form = EntryForm(data = request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id = topic_id)
+
+    # Display a blank or incalid form.
+    context = {'topic' : topic, 'form':form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """ Editng an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id= topic.id)
+
+    context = {'entry' : entry, 'topic' : topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html', context)
